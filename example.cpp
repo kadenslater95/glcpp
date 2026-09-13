c435e6a903973d78138a384962e0b4e9529ad387
Kaden Slater <kadenslater95@gmail.com>
2026-09-13 09:00:00 -0500

    Modern OpenGL Init

--- example.cpp
+++ example.cpp
@@ -1,35 +1,39 @@
-#include <GL/gl.h>
-#include <GL/glu.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+#include <GL/glew.h>
 #include <GL/glut.h>
 
+
+GLuint compileShader(const std::string, GLenum);
+void initGeometry();
+void init();
 
 void displayFunc();
 
 
-void init() {
-    glClearColor(0.0, 0.0, 0.0, 0.0);
-
-    glColor3f(1.0f, 1.0f, 1.0f);
-    glPointSize(4.0);
-
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluOrtho2D(0.0, 500.0, 0.0, 500.0);
-}
+GLuint shaderProgram, VAO, VBO;
 
 
 int main(int argc, char** argv) {
     glutInit(&argc, argv);
 
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
 
     glutInitWindowSize(500, 500);
     glutInitWindowPosition(100, 150);
     glutCreateWindow("glcpp");
 
-    glutDisplayFunc(displayFunc);
+    if(glewInit() != GLEW_OK) {
+        std::cerr << "Failed to initialize GLEW!" << "\n";
+        return -1;
+    }
 
     init();
+
+    glutDisplayFunc(displayFunc);
 
     glutMainLoop();
 
@@ -37,13 +41,76 @@ int main(int argc, char** argv) {
 }
 
 
+GLuint compileShader(std::string shaderPath, GLenum shaderType) {
+    std::ifstream shader_fstream(shaderPath);
+
+    if(!shader_fstream.is_open()) {
+        std::cerr << "Failed to open shader source file!" << "\n";
+        return 0; // Note: glCreateShader returns 0 for error
+    }
+
+    std::stringstream buffer;
+    buffer << shader_fstream.rdbuf();
+
+    std::string shaderSource = buffer.str();
+    const char *shaderSourcePtr = shaderSource.c_str();
+
+    GLuint shader = glCreateShader(shaderType);
+    glShaderSource(shader, 1, &shaderSourcePtr, NULL);
+    glCompileShader(shader);
+
+    return shader;
+}
+
+
+void initGeometry() {
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f, 0.5f, 0.0f
+    };
+
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void *) 0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+}
+
+
+void init() {
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+
+    GLuint vertexShader = compileShader("shader.vert", GL_VERTEX_SHADER);
+    GLuint fragmentShader = compileShader("shader.frag", GL_FRAGMENT_SHADER);
+
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    initGeometry();
+}
+
+
 void displayFunc() {
     glClear(GL_COLOR_BUFFER_BIT);
 
-    glBegin(GL_LINES);
-        glVertex2i(100, 50);
-        glVertex2i(100, 130);
-    glEnd();
+    glUseProgram(shaderProgram);
 
-    glFlush();
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    glutSwapBuffers();
 }
